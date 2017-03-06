d49bb5c32d5c8b08c837f8da6d175c316f64b5c8
chenjiajian <798095202@qq.com>
2017-03-06 19:57:15 +0800

    解决alias深度冲突问题

--- src/parseArgv.js
+++ src/parseArgv.js
@@ -12,21 +12,32 @@ function parseArgv (folder, argv) {
 		let dirPath = argv[i].split(":")[0].split("."),
 			newKey  = argv[i].split(":")[1].trim();
 
-		for (let j = 0, obj = folder; j < dirPath.length; j++) {
+		// 逐步走dirPath
+		for (let j = 0, obj = folder, len = dirPath.length; j < len; j++) {
 			temp = obj[dirPath[j]];
-			if (j != dirPath.length - 1) {
+			// 不是最后一步
+			if (j != len - 1) {
 				if (!temp || !isObject(temp)) {
-					throw new Error('Argv path error: ' + temp);
+					throw new Error('Procedure argv path error: ' + dirPath[j]);
 					return;
 				}
 				obj = temp;
-			} else {
+			} else {// 最后一步
 				if (!temp) {
-					throw new Error('Argv target error: ' + temp);
+					throw new Error('Final argv target error: ' + dirPath[j]);
 					return;
 				}
 				obj[newKey] = temp;
 				delete obj[dirPath[j]];
+
+				// 一旦更换文件夹名字成功，必须把接下来的含此已修改路径的alias的路径改写
+				let dirPathStr = dirPath.join('.');
+				for (let k = i + 1; k < argv.length; k++) {
+					let index = argv[k].indexOf(dirPathStr);
+					if (index >= 0 && argv[k][index + dirPathStr.length] == '.') {
+						argv[k] = argv[k].replace(dirPathStr, dirPathStr.replace(dirPath[j], newKey));
+					}
+				}
 			}
 		}
 
